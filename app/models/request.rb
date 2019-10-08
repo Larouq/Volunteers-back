b8cdc6a5266b2a929c98390a2c9b7ff2ed7c9c52
Benoit Rouquié <benoit.rouquie@adotmob.com>
2019-10-08 17:43:11 +0200

    add lenght to description

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,6 +1,6 @@
 class Request < ApplicationRecord
     validates :title, presence: true
     validates :address, presence: true
-    validates :description, presence: true
+    validates :description, presence: true, length: {in: 2..300}
     belongs_to :user
 end
