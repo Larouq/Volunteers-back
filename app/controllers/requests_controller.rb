49f7120ed6db5eb83c9d3121b7f861556b6d8cd0
Benoit <benoit@MacBook-Pro-de-Benoit.local>
2019-10-31 10:25:56 +0100

    add before action

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,5 +1,5 @@
 class RequestsController < ApplicationController
-    # before_action :authenticate_user!
+    before_action :authenticate_user, only: [:create, :update]
 
     def index
         if  params[:user_id]
--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -1,5 +1,5 @@
 class ResponsesController < ApplicationController
-    # before_action :authenticate_user!
+    before_action :authenticate_user!
 
     def index
         if params[:request_id]
